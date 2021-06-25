6568f4e33cfa9d1a8aace1632b1d1026106d0e82
trofimchuk <trofimchuk@itstep.academy>
2021-06-25 17:16:46 +0300

    Start with product

--- Shop-15/Controllers/ProductController.cs
+++ Shop-15/Controllers/ProductController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Shop_15.Data;
 using Shop_15.Models;
+using Shop_15.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,9 +22,49 @@ namespace Shop_15.Controllers
 
         public IActionResult Index()
         {
-
             IEnumerable<Product> productList = _db.Product.Include(u => u.Category);
             return View(productList);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            ProductVM productVM = new ProductVM()
+            {
+                Product = new Product(),
+                CategorySelectList = _db.Category.Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+            };
+
+            if (id == null)
+            {
+                return View(productVM);
+            }
+            else
+            {
+                productVM.Product = _db.Product.Find(id);
+                if(productVM.Product == null)
+                {
+                    return NotFound();
+                }
+
+                return View(productVM);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Product.Add(product);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
         }
     }
 }
