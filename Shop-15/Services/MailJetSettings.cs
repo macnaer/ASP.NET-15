1919130200ab264bb9ec44155770be21f780272f
k2 <trofimchuk.an@gmail.com>
2021-07-14 16:08:43 +0300

    Send mail

---
+++ Shop-15/Services/MailJetSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop_15.Services
+{
+    public class MailJetSettings
+    {
+        public string ApiKey { get; set; }
+        public string SecretKey { get; set; }
+    }
+}
